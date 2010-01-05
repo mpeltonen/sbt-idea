4bc209720a0da49f538f060b93bd4b3b8004fa45
Mikko Peltonen <mpe@L229.(none)>
2010-01-05 20:34:19 +0200

    add publishing repo

--- project/build/IdeaPluginProject.scala
+++ project/build/IdeaPluginProject.scala
@@ -1,2 +1,4 @@
 import sbt._
-class IdeaPluginProject(info: ProjectInfo) extends PluginProject(info)
+class IdeaPluginProject(info: ProjectInfo) extends PluginProject(info) {
+  val publishTo = Resolver.ssh("panda-repo", "www.laughingpanda.org", "~/public_html/repo/")
+}
