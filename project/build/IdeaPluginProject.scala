6247ec988bb70ffdd0158412a3334401b368b97d
Mikko Peltonen <mpe@L229.(none)>
2010-01-07 18:45:29 +0200

    Use official repository for publishing

--- project/build/IdeaPluginProject.scala
+++ project/build/IdeaPluginProject.scala
@@ -1,4 +1,4 @@
 import sbt._
 class IdeaPluginProject(info: ProjectInfo) extends PluginProject(info) {
-  val publishTo = Resolver.ssh("panda-repo", "www.laughingpanda.org", "~/public_html/repo/")
+  val publishTo = Resolver.ssh("panda-repo", "www.laughingpanda.org", "/var/www/localhost/htdocs/maven2/snapshots")
 }
