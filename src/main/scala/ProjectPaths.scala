cd4b647ab5773956cdbff66e978643ec152f1e16
Mikko Peltonen <mikko.peltonen@reaktor.fi>
2010-04-28 22:01:08 +0300

    Add support for ParentProjects as subprojects. Intermediate ParentProject's don't need to mix in IdeaPlugin trait, only top-level ParentProject and leaf projects need it.

--- src/main/scala/ProjectPaths.scala
+++ src/main/scala/ProjectPaths.scala
@@ -1,13 +1,19 @@
 import java.io.File
 import sbt.Configurations._
-import sbt.{Path, PathFinder, BasicDependencyProject}
+import sbt.{Path, PathFinder, BasicDependencyProject, ParentProject}
 
 trait ProjectPaths {
   val project: BasicDependencyProject
 
   def projectPath: File = project.info.projectPath.asFile
-  def childProjects: List[(String, String)] = project.subProjects.values.toList.map { s =>
-    (relativePath(s.info.projectPath.asFile), s.name)
+  def childProjects: List[(String, String)] = childProjectsOf(project)
+  private def childProjectsOf(p: BasicDependencyProject): List[(String, String)] = {
+    p.subProjects.values.toList.flatMap { s => 
+      s match {
+        case pp: ParentProject => childProjectsOf(pp)
+        case _ => (relativePath(s.info.projectPath.asFile), s.name) :: Nil
+      }
+    }
   }
   def relativePath(targetPath: File): String = {
     def pathComponentsOf(f: File): List[String] = {
