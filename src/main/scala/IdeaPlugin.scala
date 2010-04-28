a63e13a9aa191660baba094f13818d321f4c6697
Mikko Peltonen <mikko.peltonen@reaktor.fi>
2010-04-28 21:58:08 +0300

    Remove direct dependency on 'update' task for generation to be faster and for it to work offline. 'update' task needs to be executed before 'idea' manually, however, to make sure all dependencies will be included.

--- src/main/scala/IdeaPlugin.scala
+++ src/main/scala/IdeaPlugin.scala
@@ -1,7 +1,7 @@
 import sbt._
 
 trait IdeaPlugin extends BasicDependencyProject {
-  lazy val idea = task {  createIdeaProject; None  } dependsOn(update) describedAs("Creates IntelliJ IDEA project files.")
+  lazy val idea = task {  createIdeaProject; None  } describedAs("Creates IntelliJ IDEA project files.")
 
   def createIdeaProject: Unit = {
     if (info.parent.isEmpty) {
