79874bc435b9e71ae7519ab433e5440b41225eac
Mikko Peltonen <mikko.peltonen@reaktor.fi>
2010-04-29 15:10:22 +0300

    Dogfooding

--- project/build/IdeaPluginProject.scala
+++ project/build/IdeaPluginProject.scala
@@ -1,5 +1,5 @@
 import sbt._
-class IdeaPluginProject(info: ProjectInfo) extends PluginProject(info) {
+class IdeaPluginProject(info: ProjectInfo) extends PluginProject(info) with IdeaPlugin {
   override def managedStyle = ManagedStyle.Maven
   lazy val publishTo = Resolver.file("GitHub Pages", new java.io.File("../mpeltonen.github.com/maven/")) 
 }
