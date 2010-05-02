de62034d9d90f91cdf03156efe63af6f75d7b403
Mikko Peltonen <mikko.peltonen@reaktor.fi>
2010-05-02 19:20:22 +0300

    Cleanup

--- src/main/scala/IdeaPlugin.scala
+++ src/main/scala/IdeaPlugin.scala
@@ -4,10 +4,12 @@ trait IdeaPlugin extends BasicDependencyProject {
   lazy val idea = task {  createIdeaProject; None  } describedAs("Creates IntelliJ IDEA project files.")
 
   def createIdeaProject: Unit = {
-    if (info.parent.isEmpty) {
+    if (this eq rootProject) {
       new IdeaProjectDescriptor(this, log).save
       new SbtProjectDefinitionIdeaModuleDescriptor(this, log).save
     }
-    if (!this.isInstanceOf[ParentProject]) new IdeaModuleDescriptor(this, log).save
+    if (isModuleProject) new IdeaModuleDescriptor(this, log).save
   }
+    
+  def isModuleProject = !this.isInstanceOf[ParentProject]
 }
