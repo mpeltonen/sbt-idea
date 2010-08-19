476045190ed813d0224ad89cebef467d4bf78a5e
Mikko Peltonen <mikko.peltonen@reaktor.fi>
2010-08-19 23:43:01 +0300

    Remove deprecated IdeaPlugin trait. Use IdeaProject instead.

--- project/build/SbtIdeaProject.scala
+++ project/build/SbtIdeaProject.scala
@@ -6,15 +6,15 @@
 
 import sbt._
 
-class SbtIdeaProject(info:ProjectInfo) extends ParentProject(info) with IdeaPlugin {
+class SbtIdeaProject(info:ProjectInfo) extends ParentProject(info) with IdeaProject {
   override def managedStyle = ManagedStyle.Maven
   lazy val publishTo = Resolver.file("GitHub Pages", new java.io.File("../mpeltonen.github.com/maven/"))
 
   lazy val core = project("sbt-idea-core", "sbt-idea-core", new Core(_))
-  lazy val plugin = project("sbt-idea-plugin", "sbt-idea-plugin", new PluginProject(_) with IdeaPlugin, core)
-  lazy val processor = project("sbt-idea-processor", "sbt-idea-processor", new ProcessorProject(_) with IdeaPlugin, core)
+  lazy val plugin = project("sbt-idea-plugin", "sbt-idea-plugin", new PluginProject(_) with IdeaProject, core)
+  lazy val processor = project("sbt-idea-processor", "sbt-idea-processor", new ProcessorProject(_) with IdeaProject, core)
 
-  class Core(info:ProjectInfo) extends DefaultProject(info) with IdeaPlugin {
+  class Core(info:ProjectInfo) extends DefaultProject(info) with IdeaProject {
     override def unmanagedClasspath = super.unmanagedClasspath +++ info.sbtClasspath
   }
 }
