7d02293c666f4f57265d9df3cd38ff58f53edf88
Mikko Peltonen <mikko.peltonen@reaktor.fi>
2010-10-13 22:12:45 +0300

    Improve test failure reporting

--- sbt-idea-tests/src/main/scala/ScriptedTestAssertTasks.scala
+++ sbt-idea-tests/src/main/scala/ScriptedTestAssertTasks.scala
@@ -4,24 +4,27 @@ import scala.collection.jcl.Conversions._
 import org.apache.commons.io.FileUtils.listFiles
 import org.apache.commons.io.FilenameUtils.removeExtension
 import scala.xml.Utility.trim
-import scala.xml.XML
+import xml.XML
 
 trait ScriptedTestAssertTasks extends BasicScalaProject {
   lazy val assertExpectedXmlFiles = task {
     val expectedFiles = listFiles(info.projectPath.asFile, List("expected").toArray, true).toArray.map(_.asInstanceOf[File])
-    List(expectedFiles: _*).foreach { expectedFile =>
-      val actualFile = new File(removeExtension(expectedFile.getAbsolutePath))
-      if (!actualFile.exists)
-        throw new AssertionFailedException("Expected file " + actualFile.getAbsolutePath + " does not exist.")
-      val actualXml = trim(XML.loadFile(actualFile))
-      val expectedXml = trim(XML.loadFile(expectedFile))
-      log.debug("Actual: " + actualXml.toString)
-      log.debug("Expected: " + expectedXml.toString)
-      if (!actualXml.equals(expectedXml))
-        throw new AssertionFailedException("Xml file " + actualFile.getName + " does not equal expected.")
-    }
-    None
+    List(expectedFiles: _*).map(assertExpectedXml).foldLeft[Option[String]](None) { (acc, fileResult) => if (acc.isDefined) acc else fileResult }
+  }
+
+  private def assertExpectedXml(expectedFile: File):Option[String] = {
+    val actualFile = new File(removeExtension(expectedFile.getAbsolutePath))
+    if (actualFile.exists) assertExpectedXml(expectedFile, actualFile)
+    else Some("Expected file " + actualFile.getAbsolutePath + " does not exist.")
+  }
+
+  private def assertExpectedXml(expectedFile: File, actualFile: File): Option[String] = {
+    val actualXml = trim(XML.loadFile(actualFile))
+    val expectedXml = trim(XML.loadFile(expectedFile))
+    if (!actualXml.equals(expectedXml)) Some(
+      "Xml file " + actualFile.getName + " does not equal expected:"
+      + "\n********** Expected **********\n " + expectedXml.toString
+      + "\n*********** Actual ***********\n " + actualXml.toString
+    ) else None
   }
 }
-
-class AssertionFailedException(msg: String) extends RuntimeException(msg) {}
