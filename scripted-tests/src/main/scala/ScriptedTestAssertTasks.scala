94ed9026b7ba48a56bf0929fdd8951ccce7a88fe
Mikko Peltonen <mikko.peltonen@reaktor.fi>
2010-10-13 18:13:11 +0300

    Fix test to work with directory based format.

--- scripted-tests/src/main/scala/ScriptedTestAssertTasks.scala
+++ scripted-tests/src/main/scala/ScriptedTestAssertTasks.scala
@@ -10,12 +10,13 @@ trait ScriptedTestAssertTasks extends BasicScalaProject {
   lazy val assertExpectedXmlFiles = task {
     val expectedFiles = listFiles(info.projectPath.asFile, List("expected").toArray, true).toArray.map(_.asInstanceOf[File])
     List(expectedFiles: _*).foreach { expectedFile =>
-      println("Expected: " + expectedFile.getAbsolutePath)
       val actualFile = new File(removeExtension(expectedFile.getAbsolutePath))
       if (!actualFile.exists)
         throw new AssertionFailedException("Expected file " + actualFile.getAbsolutePath + " does not exist.")
       val actualXml = trim(XML.loadFile(actualFile))
       val expectedXml = trim(XML.loadFile(expectedFile))
+      log.debug("Actual: " + actualXml.toString)
+      log.debug("Expected: " + expectedXml.toString)
       if (!actualXml.equals(expectedXml))
         throw new AssertionFailedException("Xml file " + actualFile.getName + " does not equal expected.")
     }
