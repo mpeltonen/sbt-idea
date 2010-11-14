ab7045803cb36012e7828e0656d2d03fdee1a409
Mikko Peltonen <mikko.peltonen@reaktor.fi>
2010-11-14 11:05:30 +0200

    Change plugin version in scripted tests

--- sbt-idea-tests/src/sbt-test/idea-plugin/single-module-with-defaults/project/plugins/Plugins.scala
+++ sbt-idea-tests/src/sbt-test/idea-plugin/single-module-with-defaults/project/plugins/Plugins.scala
@@ -1,7 +1,7 @@
 import sbt._
  
 class Plugins(info: ProjectInfo) extends PluginDefinition(info) {
-  val testedVersion = "0.1-SNAPSHOT"
+  val testedVersion = "0.2-SNAPSHOT"
   val groupId = "com.github.mpeltonen"
   val ideaPlugin = groupId % "sbt-idea-plugin" % testedVersion
   val scriptedTestUtils = groupId % "sbt-idea-tests_2.7.7" % testedVersion
