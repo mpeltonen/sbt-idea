eee2ca55649c0ed7ee6ac58e374a1c895d536964
Mikko Peltonen <mikko.peltonen@reaktor.fi>
2011-02-21 19:50:47 +0200

    Add exclusions defined by excluded.folders property also to ParentProject .iml descriptor. Fixes #47.

--- sbt-idea-core/src/main/scala/ParentProjectIdeaModuleDescriptor.scala
+++ sbt-idea-core/src/main/scala/ParentProjectIdeaModuleDescriptor.scala
@@ -8,13 +8,21 @@ import sbt._
 import xml.Node
 
 class ParentProjectIdeaModuleDescriptor(val project: ParentProject, val log: Logger) extends SaveableXml with ProjectPaths {
+  val env = new IdeaEnvironment(project.rootProject)
   val path = String.format("%s/%s.iml", projectPath, project.name)
 
   def content: Node =
    <module type="JAVA_MODULE" version="4">
       <component name="NewModuleRootManager" inherit-compiler-output="true">
         <exclude-output />
-        <content url="file://$MODULE_DIR$" />
+        <content url="file://$MODULE_DIR$">
+          {
+            env.excludedFolders.value.split(",").toList.map(_.trim).sort(_ < _).map { entry =>
+              log.info(String.format("Excluding folder %s\n", entry))
+              <excludeFolder url={String.format("file://$MODULE_DIR$/%s", entry)} />
+            }
+          }
+        </content>
         <orderEntry type="inheritedJdk" />
         <orderEntry type="sourceFolder" forTests="false" />
       </component>
