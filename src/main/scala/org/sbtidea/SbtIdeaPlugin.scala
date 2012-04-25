278f1b64d325ba58e72e1568b3b79ecea826bfa4
Mikko Peltonen <mikko.peltonen@reaktor.fi>
2012-04-25 10:47:52 +0300

    Include classes and sources of ResolvedProject.dependencies (Compile)
    Include compile target directories (+ sources) of ResovedProject.dependencies
    in Idea dependencies. These include for example source dependencies from
    git declared like: ...dependsOn(uri("git://github.com/...

--- src/main/scala/org/sbtidea/SbtIdeaPlugin.scala
+++ src/main/scala/org/sbtidea/SbtIdeaPlugin.scala
@@ -136,15 +136,15 @@ object SbtIdeaPlugin extends Plugin {
   def projectData(projectRef: ProjectRef, project: ResolvedProject, buildStruct: BuildStructure,
                   state: State, args: Seq[String]): SubProjectInfo = {
 
-    def optionalSetting[A](key: ScopedSetting[A]) : Option[A] = key in projectRef get buildStruct.data
+    def optionalSetting[A](key: ScopedSetting[A], pr: ProjectRef = projectRef) : Option[A] = key in pr get buildStruct.data
 
     def logErrorAndFail(errorMessage: String): Nothing = {
       logger(state).error(errorMessage);
       throw new IllegalArgumentException()
     }
 
-    def setting[A](key: ScopedSetting[A], errorMessage: => String) : A = {
-      optionalSetting(key) getOrElse {
+    def setting[A](key: ScopedSetting[A], errorMessage: => String, pr: ProjectRef = projectRef) : A = {
+      optionalSetting(key, pr) getOrElse {
         logErrorAndFail(errorMessage)
       }
     }
@@ -216,7 +216,10 @@ object SbtIdeaPlugin extends Plugin {
       }
     )
     val basePackage = setting(ideaBasePackage, "missing IDEA base package")
-    SubProjectInfo(baseDirectory, projectName, project.uses.map(_.project).toList, compileDirectories,
+    val classpathDeps = project.dependencies.map { dep =>
+      (setting(Keys.classDirectory in Compile, "Missing class directory", dep.project), setting(Keys.sourceDirectories in Compile, "Missing source directory", dep.project))
+    }
+    SubProjectInfo(baseDirectory, projectName, project.uses.map(_.project).toList, classpathDeps, compileDirectories,
       testDirectories, librariesExtractor.allLibraries, scalaInstance, ideaGroup, None, basePackage)
   }
 
