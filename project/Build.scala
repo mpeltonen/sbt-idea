92a51caa2195924091f84e98befefb86c276b7ae
Mikko Peltonen <mikko.peltonen@reaktor.fi>
2012-05-11 23:43:59 +0300

    Add Sonatype publishing config

--- project/Build.scala
+++ project/Build.scala
@@ -10,7 +10,14 @@ object SbtIdeaBuild extends Build {
     name := "sbt-idea",
     version := "1.1.0-SNAPSHOT",
     publishTo := Some(Resolver.file("Github Pages", Path.userHome / "git" / "mpeltonen.github.com" / "maven" asFile)(Patterns(true, Resolver.mavenStyleBasePattern))),
+    publishTo <<= version { (v: String) =>
+      val nexus = "https://oss.sonatype.org/"
+      if (v.trim.endsWith("SNAPSHOT")) Some("snapshots" at nexus + "content/repositories/snapshots")
+      else Some("releases" at nexus + "service/local/staging/deploy/maven2")
+    },
     publishMavenStyle := true,
+    publishArtifact in Test := false,
+    pomExtra := extraPom,
     resolvers += Classpaths.typesafeSnapshots,
     scalacOptions ++= Seq("-deprecation", "-unchecked"),
     libraryDependencies ++= scriptedTestHelperDependencies
@@ -19,4 +26,25 @@ object SbtIdeaBuild extends Build {
   private def scriptedTestHelperDependencies = Seq(
     "commons-io" % "commons-io" % "2.0.1"
   )
+
+  def extraPom = (
+    <url>http://your.project.url</url>
+    <licenses>
+      <license>
+        <name>BSD-style</name>
+        <url>http://www.opensource.org/licenses/BSD-3-Clause</url>
+        <distribution>repo</distribution>
+      </license>
+    </licenses>
+    <scm>
+      <url>git@github.com:mpeltonen/sbt-idea.git</url>
+      <connection>scm:git:git@github.com:mpeltonen/sbt-idea.git</connection>
+    </scm>
+    <developers>
+      <developer>
+      <id>mpeltonen</id>
+      <name>Mikko Peltonen</name>
+      <url>http://github.com/mpeltonen</url>
+    </developer>
+  </developers>)
 }
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,6 +1,7 @@
 resolvers ++= Seq(
-  Classpaths.typesafeSnapshots,
   "sbt-idea-repo" at "http://mpeltonen.github.com/maven/"
 )
+
+addSbtPlugin("com.jsuereth" % "xsbt-gpg-plugin" % "0.6", sbtVersion = "0.12.0-Beta2")
 
 libraryDependencies <+= sbtVersion("org.scala-sbt" % "scripted-plugin" % _)
