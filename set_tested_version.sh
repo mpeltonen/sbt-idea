a4ae3ed4dc6566382bd8bb73b3016689ad1076cc
Mikko Peltonen <mikko.peltonen@reaktor.fi>
2012-08-07 09:25:27 +0300

    Fix script for osx

--- set_tested_version.sh
+++ set_tested_version.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 for f in $(find $(dirname $0)/src/sbt-test -name plugins.sbt); do
-  sed -i 's/\("sbt-idea" % "\)\(.*\)"/\1'$1'"/' $f
+  sed -i '' 's/\("sbt-idea" % "\)\(.*\)"/\1'$1'"/' $f
 done
