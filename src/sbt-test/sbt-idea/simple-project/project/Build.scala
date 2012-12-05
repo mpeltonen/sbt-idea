a9162a7b61f01fc9bbae376eb4f1307ea39f054f
Mikko Peltonen <mikko.peltonen@reaktor.fi>
2012-12-05 13:53:22 +0200

    Add test for #186

--- src/sbt-test/sbt-idea/simple-project/project/Build.scala
+++ src/sbt-test/sbt-idea/simple-project/project/Build.scala
@@ -5,6 +5,12 @@ import sbt.Keys._
 object ScriptedTestBuild extends AbstractScriptedTestBuild("simple-project") {
   
   lazy val root = Project("main", file("."), settings = Defaults.defaultSettings ++ scriptedTestSettings ++ Seq(
-    libraryDependencies += "junit" % "junit" % "4.8.2", scalacOptions ++= Seq("-unchecked", "-deprecation")
+    libraryDependencies ++= dependencies, scalacOptions ++= Seq("-unchecked", "-deprecation")
   ))
+
+  lazy val dependencies = Seq(
+    "junit" % "junit" % "4.8.2",
+    "org.eclipse.jetty" % "jetty-server" % "7.0.0.v20091005" % "test",
+    "javax.servlet" % "servlet-api" % "2.5" % "provided"
+  )
 }
