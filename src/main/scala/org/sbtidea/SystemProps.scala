b9047a7eb908d7a8f16a2b1c2fd18058e335aa16
Mikko Peltonen <mikko.peltonen@reaktor.fi>
2012-12-14 16:24:13 +0200

    Normalize user.home value (#189)

--- src/main/scala/org/sbtidea/SystemProps.scala
+++ src/main/scala/org/sbtidea/SystemProps.scala
@@ -4,5 +4,8 @@ object SystemProps {
   val jdkName = System.getProperty("java.version").substring(0, 3)
   val languageLevel = "JDK_" + jdkName.replace(".", "_")
   val runsOnWindows = System.getProperty("os.name").contains("Windows")
-  val userHome = System.getProperty("user.home")
+  val userHome = {
+    val system = System.getProperty("user.home")
+    if (system.endsWith(java.io.File.separator)) system.dropRight(1) else system
+  }
 }
