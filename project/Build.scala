b0c0655c9b5a1bcb38a9e6fb0d2376c2b8ec3b15
Mikko Peltonen <mikko.peltonen@reaktor.fi>
2013-07-28 00:28:40 +0300

    Set version 1.5.1

--- project/Build.scala
+++ project/Build.scala
@@ -8,7 +8,7 @@ object SbtIdeaBuild extends Build with BuildExtra {
     sbtPlugin := true,
     organization := "com.github.mpeltonen",
     name := "sbt-idea",
-    version := "1.6.0-SNAPSHOT",
+    version := "1.5.1",
     sbtVersion in Global := "0.13.0-RC3",
     scalaVersion in Global := "2.10.2",
     publishTo := Some(Resolver.file("Github Pages", Path.userHome / "git" / "mpeltonen.github.com" / "maven" asFile)(Patterns(true, Resolver.mavenStyleBasePattern))),
